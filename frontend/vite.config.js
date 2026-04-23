88c25f38da5e34031cb51cb9e28188c823199101
Gonzalo Aliaga <gonz.aliaga@duocuc.cl>
2026-04-23 14:21:50 -0400

    frontend base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,3 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+// COMUNICACIÓN LOCAL CON SERVICIO AUTH
+server: {
+  proxy: {
+    '/api'; 'http://localhost:8080'
+  }
+}
