1804937229e3a5dbcce76abbf0a439086300be03
gonzaloaliaga <gonz.aliaga@duocuc.cl>
2026-05-08 09:00:41 -0400

    fix cors config

--- auth-service/src/main/kotlin/cl/donaton/donaton/controller/AuthController.kt
+++ auth-service/src/main/kotlin/cl/donaton/donaton/controller/AuthController.kt
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.CrossOrigin
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = ["http://localhost:8080", "http://localhost:5173"])
 class AuthController(private val userRepository: UserRepository) {
 
     // Elegimos la estrategia (Podría inyectarse por configuración)
