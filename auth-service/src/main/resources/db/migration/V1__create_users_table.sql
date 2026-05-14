2b7ce743c6229e3cd1c8e5c157af93f178e66020
Gonzalo Aliaga <gonz.aliaga@duocuc.cl>
2026-05-13 22:43:11 -0400

    fix rol en flyway v1 users

--- auth-service/src/main/resources/db/migration/V1__create_users_table.sql
+++ auth-service/src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,11 +1,10 @@
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO users (username, password, role) VALUES ('admin', '1111', 'ADMIN');
-INSERT INTO users (username, password, role) VALUES ('logistica', '2222', 'LOGISTIC');
-INSERT INTO users (username, password, role) VALUES ('voluntario', '3333', 'VOLUNTEER');
-INSERT INTO users (username, password, role) VALUES ('donante', '4444', 'DONOR');
+INSERT INTO users (username, password) VALUES ('admin', '1111');
+INSERT INTO users (username, password) VALUES ('logistica', '2222');
+INSERT INTO users (username, password) VALUES ('voluntario', '3333');
+INSERT INTO users (username, password) VALUES ('donante', '4444');
